9461dc8f4ab731aae35b42837d0be25a7fc0d86e
Marek Ostafin <marek.ostafin16@gmail.com>
2024-03-31 22:17:43 +0200

    zadanie1

--- zadanie1/zadanie1.pas
+++ zadanie1/zadanie1.pas
@@ -9,14 +9,11 @@ var
     i, r: Integer;
 begin
     Randomize;
-    writeln('========== Zadanie 1a ==========');
-    writeln('Losowe liczby:');
     setlength(rnd_arr, num);
     size := num;
     for i := 0 to num-1 do
     begin
         r := min + Random(max-min);
-        writeln(i, ': ', r);
         rnd_arr[i] := r;
     end;
 end;
@@ -25,7 +22,7 @@ procedure Zadanie1b;
 var
     i, j, tmp: Integer;
 begin
-    writeln('========== Zadanie 1b ==========');
+
     for i := 0 to size-1 do
     begin
         for j := 0 to size-i-1 do
@@ -38,16 +35,128 @@ begin
             end;
         end;
     end;
-    writeln('Posortowane liczby:');
-    for i := 1 to size do
-        writeln(i, ': ', rnd_arr[i]);
 end;
 
+// Testy jednostkowe
+function Zadanie1a_Test1():integer;
+var
+  min, max, num: Integer;
+begin
+    min := 50;
+    max := 100;
+    num := 10;
+    Zadanie1a(min, max, num);
+
+    if num = high(rnd_arr)-low(rnd_arr)+1 then
+    begin
+        writeln('[1/5] Zadanie1a_Test1 PASSED');
+        Exit(1);
+    end;
+    writeln('[1/5] Zadanie1a_Test1 FAILED');
+    Exit(0);            
+end;
+
+function Zadanie1a_Test2():integer;
+var
+  min, max, num, i: Integer;
+begin
+    min := 50;
+    max := 100;
+    num := 10;
+    Zadanie1a(min, max, num);
+
+    for i := 0 to num-1 do
+    begin
+        if (rnd_arr[i] < min) or (rnd_arr[i] > max) then
+        begin
+            writeln('[2/5] Zadanie1a_Test2 FAILED');
+            Exit(0);
+        end;
+    end;
+    writeln('[2/5] Zadanie1a_Test2 PASSED');
+    Exit(1);            
+end;
+
+function Zadanie1b_Test1():Integer;
+var
+    i: Integer;
+begin
+    Zadanie1b;
+    for i := 1 to size-1 do
+    begin
+        if rnd_arr[i-1] > rnd_arr[i] then
+        begin
+            writeln('[3/5] Zadanie1b_Test1 FAILED');
+            Exit(0);
+        end;
+    end;
+    writeln('[3/5] Zadanie1b_Test1 PASSED');
+    Exit(1);
+end;
+
+function Zadanie1b_Test2():Integer;
+var
+    i: Integer;
+    unsorted_array: array[0..9] of integer = (50,30,91,40,80,60,90,10,71,20);
+    sorted_array: array[0..9] of integer = (10,20,30,40,50,60,71,80,90,91);
+begin
+    size := 10;
+    setlength(rnd_arr, size);
+    for i := 0 to 9 do
+    begin
+        rnd_arr[0] := unsorted_array[0];
+    end;
+        
+    Zadanie1b;
+    for i := 0 to size-1 do
+    begin
+        if rnd_arr[i] <> sorted_array[i] then
+        begin
+            writeln('[4/5] Zadanie1b_Test2 FAILED');
+            Exit(0);
+        end;
+    end;
+    writeln('[4/5] Zadanie1b_Test2 PASSED');
+    Exit(1);
+end;
+
+function Zadanie1b_Test3():Integer;
+var
+    i: Integer;
+    unsorted_array: array[0..9] of integer = (50,30,91,40,80,60,90,10,71,20);
+    sorted_array: array[0..9] of integer = (10,20,30,40,50,60,71,80,90,91);
+begin
+    rnd_arr := unsorted_array;
+    Zadanie1b;
+    for i := 0 to size-1 do
+    begin
+        if rnd_arr[i] <> sorted_array[i] then
+        begin
+            writeln('[5/5] Zadanie1b_Test3 FAILED');
+            Exit(0);
+        end;
+    end;
+    writeln('[5/5] Zadanie1b_Test3 PASSED');
+    Exit(1);
+end;
+
+procedure runTests;
+var
+    passed: Integer;
+begin
+    passed := 0;
+    passed += Zadanie1a_Test1;
+    passed += Zadanie1a_Test2;
+    passed += Zadanie1b_Test1;
+    passed += Zadanie1b_Test2;
+    // passed += Zadanie1b_Test3;
+
+    writeln('==========================');
+    writeln(passed, '/5 tests passed');
+    writeln('==========================');
+end;
+//
 
 begin
-    minVal := 11;
-    maxVal := 15;
-    numVal := 10;
-    Zadanie1a(minVal, maxVal, numVal);
-    Zadanie1b;
+    runTests;
 end.
